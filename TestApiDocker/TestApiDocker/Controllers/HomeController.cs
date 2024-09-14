892610bd8cfb701c820134d9f5aea5195452a5e4
Vipul Malhotra <vipulm124@gmail.com>
2024-09-15 00:08:05 +0530

    added small proj of reactjs python and netcore with Dockerfiles

---
+++ TestApiDocker/TestApiDocker/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApiDocker.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    
+
+    [HttpGet(Name = "GetHomeData")]
+    public string Get()
+    {
+        return "This is a response";
+    }
+}
+
---
+++ TestApiDocker/TestApiDocker/Program.cs
@@ -0,0 +1,28 @@
+﻿using TestApiDocker;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.Urls.Add("http://0.0.0.0:80");
+app.Run();
+
